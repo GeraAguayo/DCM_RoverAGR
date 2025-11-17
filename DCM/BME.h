c600baa41f9270059b98fd702fa113333f45a629
Gerardo Aguayo <geraaguayoc@gmail.com>
2025-11-17 10:07:24 -0600

    Implemented BME 680 sensor functionality, corrected pressure bug

---
+++ DCM/BME.h
@@ -0,0 +1,31 @@
+#ifndef BME_H
+#define BME_H
+
+//BME 380 Sensor module
+//GRI
+
+#include <Wire.h>
+//#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+struct bme_data  {
+  float temperature; //°C
+  float pressure; //hPa
+  float humidity; //%
+  float gas; //kOhms
+  float altitude; //m
+};
+
+class BME{
+  private:
+    Adafruit_BME680 bme;
+  public:
+    BME();
+    ~BME();
+    void begin();
+    bme_data* get_values();
+    bool is_enabled();
+};
+
+#endif
--- DCM/DCM.ino
+++ DCM/DCM.ino
@@ -1,48 +1,94 @@
-//DATA COLLECTION MODULE
-//Rover AGR
-//Gerardo Aguayo
-#include "BMP.h"
+/*
+DATA COLLECTION MODULE
+Rover AGR
+Gerardo Aguayo
+*/
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include "BME.h"
+BME bme_sensor;
+//GPS config
+#define GPS_TX_PIN 4
+#define GPS_RX_PIN 3
+SoftwareSerial gps_serial(GPS_TX_PIN,GPS_RX_PIN);
+TinyGPSPlus gps;
 
-BMP bmp_sensor;
+/*
+Serial Values Standard
+Temperature - 0
+Pressure - 1
+Altitude - 2
+Humidity - 3
+Gas - 4
+Latitude - 5
+Longitude - 6
+*/
+float temperature, pressure, altitude, humidity, gas, latitude, longitude = 0.0;
+bool valid_data = true;
 
 void raise_error();
 
 enum error_ids {
-  BMP_ERROR = 2,
+  BME_ERROR = 2,
 };
 
 void setup() {
   Serial.begin(9600);
   delay(100);
-  bmp_sensor.begin();
+  bme_sensor.begin();
+  delay(100);
+  gps_serial.begin(9600);
 }
 
-bool set_up = false;
-
-//Serial Values Standard
-//Temperature val - 0
-//Pressure val - 1
-//Altitude val - 2
-
-
 void loop() {
-  if (bmp_sensor.is_enabled()){
-    bmp_data* data = bmp_sensor.read_values();
-    Serial.println("DATA");
-    Serial.println(data->temperature);
-    Serial.println(data->pressure);
-    Serial.println(data->altitude);
-    Serial.println("END");
+
+  //Read GPS
+  while (gps_serial.available()){
+    gps.encode(gps_serial.read());
+  }
+  if (gps.location.isValid()){
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+  }
+  else{
+    latitude = 0.0;
+    longitude = 0.0;
+  }
+
+  //Read bme680
+  if (bme_sensor.is_enabled()){
+    bme_data* data = bme_sensor.get_values();
+    temperature = data->temperature;
+    pressure = data->pressure;
+    altitude = data->altitude;
+    humidity = data->humidity;
+    gas = data->gas;
+    valid_data = true;
     delete data;
   }
   else{
-    //Raise BMP error and try again
-    raise_error(BMP_ERROR);
-    bmp_sensor.begin();
+    //Raise BME error and try again
+    raise_error(BME_ERROR);
+    bme_sensor.begin();
+  }
+
+  if (valid_data){
+    //Send data
+    Serial.println("DATA");
+    Serial.println(temperature);
+    Serial.println(pressure);
+    Serial.println(altitude);
+    Serial.println(humidity);
+    Serial.println(gas);
+    Serial.println(latitude);
+    Serial.println(longitude);
+    Serial.println("END");
+
   }
 }
 
 void raise_error(error_ids id){
+  valid_data = false;
   Serial.println("SYSLOG");
   Serial.println(id);
   Serial.println("END");
